3ea6cba4fb53cbdf03ce60ddaf81a6ad378890ac
Pedro Lima <contact.pedrolima@gmail.com>
2022-12-13 09:50:27 -0300

    added pokeapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pokemon, setPokemon] = useState<any>();
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+      .then((response) => response.json())
+      .then((data) => (
+        console.log(data),
+        setPokemon(data)
+      ))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
+    <div className="container">
       <h1>Mathias site</h1>
+      {pokemon && <img src={pokemon.sprites.front_default} alt="" />}
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
