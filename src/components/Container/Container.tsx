73f073bf91776776a0b852f67c7ecbc70482a117
Pedro Lima <contact.pedrolima@gmail.com>
2022-12-13 20:29:58 -0300

    image update

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -3,10 +3,10 @@ import { LittleLegend } from "../LittleLegend/LittleLegend";
 export function Container () {
     return (
         <main className="flex flex-1 w-full items-center justify-center">
-            <LittleLegend image="public/Featherknight_Spirit_Blossom_Kami_Tier_1.png"/>
-            <LittleLegend image="public/Featherknight_Pengu_Tier_2.png"/>
-            <LittleLegend image="public/Featherknight_Spirit_Blossom_Yokai_Tier_2.png"/>
-            <LittleLegend image="public/Featherknight_Terra_Inktales_Tier_2.png"/>
+            <LittleLegend image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Spirit_Blossom_Kami_Tier_1.png"/>
+            <LittleLegend image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Pengu_Tier_2.png"/>
+            <LittleLegend image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Spirit_Blossom_Yokai_Tier_2.png"/>
+            <LittleLegend image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Terra_Inktales_Tier_2.png"/>
         </main>
     );
 };
