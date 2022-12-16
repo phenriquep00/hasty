b38e02dc51b308c1d44aa641031719e537f066ce
Pedro Lima <contact.pedrolima@gmail.com>
2022-12-16 18:11:43 -0300

    container update

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,12 +1,31 @@
 import { LittleLegend } from "../LittleLegend/LittleLegend";
 
-export function Container () {
-    return (
-        <main className="flex flex-1 w-full items-end justify-center">
-            <LittleLegend image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Spirit_Blossom_Kami_Tier_1.png"/>
-            <LittleLegend image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Pengu_Tier_2.png"/>
-            <LittleLegend image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Spirit_Blossom_Yokai_Tier_2.png"/>
-            <LittleLegend image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Terra_Inktales_Tier_2.png"/>
-        </main>
-    );
-};
+
+export function Container() {
+  return (
+    <main className="flex flex-1 w-full items-end justify-center">
+      <div className="mb-10"> 
+        <LittleLegend
+          text={"sobre"}
+          image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Spirit_Blossom_Kami_Tier_1.png"
+        />
+      </div>
+
+      <LittleLegend
+        text={"projetos"}
+        image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Pengu_Tier_2.png"
+      />
+      <LittleLegend
+        text={"suporte"}
+        image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Spirit_Blossom_Yokai_Tier_2.png"
+      />
+      <div className="mb-10"> 
+      <LittleLegend
+        text={"alguma coisa"}
+        image="https://raw.githubusercontent.com/phenriquep00/hasty/main/public/Featherknight_Terra_Inktales_Tier_2.png"
+      />
+      </div>
+
+    </main>
+  );
+}
--- src/components/LittleLegend/LittleLegend.tsx
+++ src/components/LittleLegend/LittleLegend.tsx
@@ -1,11 +1,19 @@
 interface ILittleLegend {
-    image: string;
+  image: string;
+  text: string;
 }
 
-export function LittleLegend({image}: ILittleLegend) {
-    return (
-        <button className="hover:scale-125 z-20 transition-transform ease-in-out duration-200">
-            <img className="animate-littleLegends" src={image} alt="little legend" />
-        </button>
-    )
+export function LittleLegend({ image, text }: ILittleLegend) {
+  return (
+    <button className="flex flex-col items-center">
+      <img
+        className="animate-littleLegends hover:scale-125 z-20 transition-transform ease-in-out duration-200"
+        src={image}
+        alt="little legend"
+      />
+
+      <p className="text-2xl capitalize font-semibold underline underline-offset-2">{text}</p>
+      
+    </button>
+  );
 }
