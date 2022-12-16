3b734f16f722b449da64b39c30c8a671e63b9606
Pedro Lima <contact.pedrolima@gmail.com>
2022-12-16 19:32:43 -0300

    anchor updates

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,25 +8,25 @@ import {
 export function Footer() {
   return (
     <footer className="flex w-full items-end justify-around mb-4 h-[15%]">
-      <button className="flex justify-center gap-2 text-[#5e5c64] hover:text-white transition-colors duration-200 ease-in-out">
+      <a href="https://www.twitch.tv/sukesvs" target={"_blank"} className="flex justify-center gap-2 text-[#5e5c64] hover:text-white transition-colors duration-200 ease-in-out">
         <TwitchLogo size={20} />
         twitch
-      </button>
+      </a >
 
-      <button className="flex justify-center gap-2 text-[#5e5c64] hover:text-white transition-colors duration-200 ease-in-out">
+      <a href="" className="flex justify-center gap-2 text-[#5e5c64] hover:text-white transition-colors duration-200 ease-in-out">
         <InstagramLogo size={20} />
         instagram
-      </button>
+      </a >
 
-      <button className="flex justify-center gap-2 text-[#5e5c64] hover:text-white transition-colors duration-200 ease-in-out">
+      <a href="" className="flex justify-center gap-2 text-[#5e5c64] hover:text-white transition-colors duration-200 ease-in-out">
         <YoutubeLogo size={20} />
         youtube
-      </button>
+      </a >
 
-      <button className="flex justify-center gap-2 text-[#5e5c64] hover:text-white transition-colors duration-200 ease-in-out">    
+      <a href="https://discord.gg/gyWQwWjnhV" target="_blank" className="flex justify-center gap-2 text-[#5e5c64] hover:text-white transition-colors duration-200 ease-in-out">    
         <DiscordLogo size={20} />
         discord
-      </button>
+      </a >
     </footer>
   );
 }
