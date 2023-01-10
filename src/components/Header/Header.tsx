39e0ce33719c756ae7963c7937ef0ef6a30dffd8
Pedro Lima <contact.pedrolima@gmail.com>
2023-01-09 21:52:31 -0300

    home page style update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 export function Header() {
   return (
-    <header className="flex w-full gap-10 items-center justify-center mt-4 h-[10%] text-4xl font-black z-10 animate-header">
+    <header className="flex w-full gap-10 items-center justify-center bg-gradient-to-b from-black to-transparent h-[10%] text-4xl font-black z-10 animate-header">
       <img
         src="/Kombi_2.png"
-        className="w-40"
+        className="w-40 mt-5"
         alt=""
       />
-      <h1 className="font-black text-[80px] bg-gradient-to-r from-[#A970FF] via-violet-400 to-pink-300 bg-clip-text text-transparent leading-loose">Kombi gaming</h1>
+      <h1 className="font-black mt-5 text-[80px] bg-gradient-to-r from-[#A970FF] via-violet-400 to-pink-300 bg-clip-text text-transparent leading-loose">Kombi gaming</h1>
     </header>
   );
 }
