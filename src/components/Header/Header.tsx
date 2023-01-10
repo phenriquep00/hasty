102647f049a16f09f40747d01f080f49027ee352
Pedro Lima <contact.pedrolima@gmail.com>
2023-01-09 22:01:09 -0300

    header style update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 export function Header() {
   return (
     <header className="flex w-full bg-gradient-to-b from-black to-transparent h-[10%] text-4xl font-black z-10">
-      <div className="flex w-full items-center justify-center gap-10 animate-header">
-        <img src="/Kombi_2.png" className="w-40 mt-5" alt="" />
-        <h1 className="font-black mt-5 text-[80px] bg-gradient-to-r from-[#A970FF] via-violet-400 to-pink-300 bg-clip-text text-transparent leading-loose">
+      <div className="flex w-full items-center justify-center gap-10 animate-header mt-8">
+        <img src="/Kombi_2.png" className="w-40" alt="" />
+        <h1 className="font-black text-[80px] bg-gradient-to-r from-[#A970FF] via-violet-400 to-pink-300 bg-clip-text text-transparent leading-loose">
         Kombi gaming
       </h1>
       </div>
