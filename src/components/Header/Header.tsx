fdd56bbf9673651d079164566ac14a605ab9bdec
Pedro Lima <contact.pedrolima@gmail.com>
2023-01-09 21:58:50 -0300

    header animation update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 export function Header() {
   return (
-    <header className="flex w-full gap-10 items-center justify-center bg-gradient-to-b from-black to-transparent h-[10%] text-4xl font-black z-10 animate-header">
-      <img
-        src="/Kombi_2.png"
-        className="w-40 mt-5"
-        alt=""
-      />
-      <h1 className="font-black mt-5 text-[80px] bg-gradient-to-r from-[#A970FF] via-violet-400 to-pink-300 bg-clip-text text-transparent leading-loose">Kombi gaming</h1>
+    <header className="flex w-full bg-gradient-to-b from-black to-transparent h-[10%] text-4xl font-black z-10">
+      <div className="flex w-full items-center justify-center gap-10 animate-header">
+        <img src="/Kombi_2.png" className="w-40 mt-5" alt="" />
+        <h1 className="font-black mt-5 text-[80px] bg-gradient-to-r from-[#A970FF] via-violet-400 to-pink-300 bg-clip-text text-transparent leading-loose">
+        Kombi gaming
+      </h1>
+      </div>
+
+      
     </header>
   );
 }
